102474f9380a6fa84b0f3ba29d6320d57301506c
PabloNovaes <ppablo.0028922@gmail.com>
2024-01-31 19:04:03 -0300

    feat: add toggle theme

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,14 @@ import {
   TableRow,
 } from "@/components/ui/table"
 
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar"
+
+import { ThemeProvider } from "@/components/theme-provider"
+
 import { Toggle } from "./components/ui/toggle"
 import { Header } from "./components/Header"
 import { useState } from "react"
@@ -14,6 +22,7 @@ import { useState } from "react"
 import { Product, fixedProducts } from "./data"
 import { Badge } from "@/components/ui/badge"
 import { Filter } from "./components/Filter"
+import { SwitchTheme } from "./components/SwitchTheme"
 
 export function App() {
   const [products, setProducts] = useState(fixedProducts)
@@ -23,47 +32,64 @@ export function App() {
   }
 
   return (
-    <div className="calcHeight max-w-4xl m-auto px-4  pb-4 flex flex-col mx-auto">
-      <Header onCreateNewProduct={setProducts} products={products} />
-      <Filter products={products} onOrderProducts={orderProducts} />
-      <div className="tableWrapper border rounded-md shadow-sm overflow-auto">
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="py-1.5">
-                Nome
-              </TableHead>
-              <TableHead className="py-1.5">
-                ID
-              </TableHead>
-              <TableHead className="py-1.5">
-                <Toggle className="w-full flex">Preço</Toggle>
-              </TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {products.map(product => {
-              const { id, name, price } = product
-              const productValue = parseFloat(String(price))
-              return (
-                <TableRow className="tableRowHover" key={id}>
-                  <TableCell className="w-full">{name}</TableCell>
-                  <TableCell className="w-full" >{id}</TableCell>
-                  <TableCell className="w-full">
-                    <Badge variant={"outline"} className={productValue > 300 ? "border-red-500 w-full" : "border-green-500 w-full"}>
-                      {price.toLocaleString("pt-br", {
-                        style: "currency",
-                        currency: "BRL"
-                      })}
-                    </Badge>
-                  </TableCell>
-                </TableRow>
-              )
-            })}
-          </TableBody>
-        </Table>
+    <ThemeProvider>
+      <header className="h-[60px] border-b flex items-center px-4 justify-between">
+
+        <SwitchTheme />
+        <h1 className="text-secondary-foreground text-2xl font-extrabold">
+          <span className="text-blue-600">Ts</span>Table
+        </h1>
+
+        <div className="flex gap-5">
+          <Avatar className="">
+            <AvatarImage src="https://github.com/pablonovaes.png" alt="@shadcn" />
+            <AvatarFallback className="bg-zinc-700">PN</AvatarFallback>
+          </Avatar>
+        </div>
+
+      </header>
+      <div className="calcHeight max-w-4xl m-auto px-4 pb-4 flex flex-col mx-auto">
+        <Header onCreateNewProduct={setProducts} products={products} />
+        <Filter products={products} onOrderProducts={orderProducts} />
+        <div className="tableWrapper border rounded-md shadow-sm overflow-auto">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="py-1.5">
+                  Nome
+                </TableHead>
+                <TableHead className="py-1.5">
+                  ID
+                </TableHead>
+                <TableHead className="py-1.5">
+                  <Toggle className="w-full flex">Preço</Toggle>
+                </TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody className="text-secondary-foreground">
+              {products.map(product => {
+                const { id, name, price } = product
+                const productValue = parseFloat(String(price))
+                return (
+                  <TableRow className="tableRowHover" key={id}>
+                    <TableCell className="w-full">{name}</TableCell>
+                    <TableCell className="w-full">{id}</TableCell>
+                    <TableCell className="w-full text-red">
+                      <Badge variant={"outline"} className={productValue > 300 ? "border-red-500 w-full " : "border-green-500 w-full"}>
+                        {price.toLocaleString("pt-br", {
+                          style: "currency",
+                          currency: "BRL"
+                        })}
+                      </Badge>
+                    </TableCell>
+                  </TableRow>
+                )
+              })}
+            </TableBody>
+          </Table>
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   )
 }
 
